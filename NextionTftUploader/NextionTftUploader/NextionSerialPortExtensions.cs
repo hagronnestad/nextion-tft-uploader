90f6b89d42a8e99d196ac17ef7cfda25887fcb42
Hein Andre Grønnestad <hag@haugnett.no>
2021-01-25 12:50:20 +0100

    Added NextionSerialPortExtensions

---
+++ NextionTftUploader/NextionTftUploader/NextionSerialPortExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.IO.Ports;
+
+namespace NextionTftUploader {
+    /// <summary>
+    /// Convenient Nextion specific SerialPort extensions for reading and writing data
+    /// </summary>
+    public static class NextionSerialPortExtensions {
+
+        public static void NextionWrite(this SerialPort sp, string text) {
+            sp.Write(text);
+            sp.Write(new byte[] { 0xff, 0xff, 0xff }, 0, 3);
+        }
+
+        public static string NextionReadConnectResponse(this SerialPort sp) {
+            return sp.ReadTo(new string(new char[] { '\x3f', '\x3f', '\x3f' }));
+        }
+
+    }
+}

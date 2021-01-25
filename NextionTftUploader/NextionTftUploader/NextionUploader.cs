03286d65dd3f4e3edf4a14fc7705ed78b8d8a2f9
Hein Andre Grønnestad <hag@haugnett.no>
2021-01-25 12:51:01 +0100

    Added Nextion TFT Upload project files

---
+++ NextionTftUploader/NextionTftUploader/NextionUploader.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+
+namespace NextionTftUploader {
+    public class NextionUploader {
+
+
+        /// <summary>
+        /// Uploads a TFT file to a Nextion device using the NEXTION HMI UPLOAD PROTOCOL (v1.0)
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="port"></param>
+        /// <param name="baudRate"></param>
+        public void Upload(string fileName, string port, int baudRate) {
+            var sp = new SerialPort(port, baudRate, Parity.None, 8, StopBits.One);
+
+            sp.Open();
+
+            // NEXTION HMI UPLOAD PROTOCOL (v1.0) Reference:
+            // https://nextion.tech/2017/09/15/nextion-hmi-upload-protocol/
+
+            Console.WriteLine($"Connecting to device on serial port: {port}, Baud rate: {baudRate}...");
+
+            // The reference says: "It is suggested to send an empty instruction before the connect instruction."
+            sp.NextionWrite("");
+            sp.NextionWrite("connect");
+
+            // Read connect handshake
+            var comok = sp.NextionReadConnectResponse();
+
+            // Example response: comok 1,101,NX4024T032_011R,52,61488,D264B8204F0E1828,16777216
+            var parts = comok.Substring(6).Split(",");
+            if (parts.Length >= 7) {
+                Console.WriteLine("Successfully connected!");
+                Console.WriteLine();
+                Console.WriteLine("DEVICE INFO");
+                Console.WriteLine("============================================");
+                Console.WriteLine(parts[0] == "1" ? "Nextion model with touch panel" : "Nextion model without touch panel");
+                Console.WriteLine($"Model: {parts[2]}");
+                Console.WriteLine($"Firmware version: {parts[3]}");
+                Console.WriteLine($"MCU code: {parts[4]}");
+                Console.WriteLine($"Device Serial Number: {parts[5]}");
+                Console.WriteLine($"Flash size: {parts[6]} bytes");
+                Console.WriteLine("============================================");
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine($"Reading TFT file: {fileName}...");
+            var data = File.ReadAllBytes(fileName);
+            Console.WriteLine($"TFT file size is: {data.Length} bytes");
+
+            Console.WriteLine($"Sending upload command to device...");
+            sp.NextionWrite($"whmi-wri {data.Length},{baudRate},0");
+
+            // Wait for ready byte
+            Console.WriteLine("Waiting for device...");
+            var s = sp.ReadByte();
+
+            if (s != 0x05) {
+                Console.WriteLine("Device not ready! Exiting...");
+                sp.Close();
+                return;
+            }
+
+            // Send TFT data
+            for (int offset = 0; offset <= data.Length; offset += 4096) {
+                var length = ((data.Length - offset) >= 4096) ? 4096 : data.Length - offset;
+
+                Console.Write($"Writing {length} bytes, {offset} bytes written ...                      ");
+                Console.CursorLeft = 0;
+                sp.Write(data, offset, length);
+
+                // The reference for NEXTION HMI UPLOAD PROTOCOL (v1.0) doesn't
+                // specifically mention it, but we need to wait for 0x05 return byte before
+                // sending the next chunk. This IS mentioned in the v1.1 reference.
+                var np = sp.ReadByte();
+
+                if (np != 0x05) {
+                    Console.WriteLine("Device not ready for more data! Exiting...");
+                    sp.Close();
+                    return;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("TFT data written successfully! Device will restart...");
+            Console.WriteLine();
+            sp.Close();
+        }
+
+    }
+}
---
+++ NextionTftUploader/NextionTftUploader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NextionTftUploader {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            if (args.Length < 3) {
+                Console.WriteLine("Too few arguments!");
+                Console.WriteLine("Expecting: {TFT file} {COM port} {Baud rate}");
+                return;
+            }
+
+            var u = new NextionUploader();
+
+            var file = args[0];
+            var port = args[1];
+            var baudRate = int.Parse(args[2]);
+
+            u.Upload(file, port, baudRate);
+        }
+
+    }
+
+}
